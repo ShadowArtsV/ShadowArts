9e183ff718770f930c36b8629cc85df4e6b6b2b5
Vaishnavi-Pashikanti <vaishamsini@gmail.com>
2025-03-20 22:26:35 +0530

    Updates

---
+++ precache-manifest.f4acaafcec53d78db606ccf8dd346df3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a82b52fd0d01a2a0447716ba5500e87e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d99bcdcea27d884bca47",
+    "url": "/static/css/main.628d5fb5.chunk.css"
+  },
+  {
+    "revision": "07e52c3b38265919d9a1",
+    "url": "/static/js/2.b5c7a794.chunk.js"
+  },
+  {
+    "revision": "9cdd264d58821d8a8819662b3b7dbcdb",
+    "url": "/static/js/2.b5c7a794.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "962d757e2961d40353ad",
+    "url": "/static/js/3.4d243666.chunk.js"
+  },
+  {
+    "revision": "d99bcdcea27d884bca47",
+    "url": "/static/js/main.17d1f6a7.chunk.js"
+  },
+  {
+    "revision": "0a23bab06fbab790e6a7",
+    "url": "/static/js/runtime-main.f38397ef.js"
+  }
+]);
