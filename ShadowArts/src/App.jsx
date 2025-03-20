192a1d9556ddbf76653f6223ec4f52f4cbb8b099
Vaishnavi-Pashikanti <139231815+Vaishnavi-Pashikanti@users.noreply.github.com>
2025-03-20 19:39:12 +0530

    Update and rename App.js to App.jsx

---
+++ ShadowArts/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import ShadowArts from './components/ShadowArts';
+
+function App() {
+  return (
+    <div className="App">
+      <ShadowArts />
+    </div>
+  );
+}
+
+export default App;
