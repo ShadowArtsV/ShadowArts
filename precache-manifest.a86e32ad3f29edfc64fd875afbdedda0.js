27d881187d0f4974ce71e8489f8544c5bf2dbd33
Vaishnavi-Pashikanti <vaishamsini@gmail.com>
2025-03-20 23:07:44 +0530

    Updates

---
+++ precache-manifest.a86e32ad3f29edfc64fd875afbdedda0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98082ff178f10a2da1e66ec82d12bb84",
+    "url": "/ShadowArts/index.html"
+  },
+  {
+    "revision": "845e8d27723635e15c74",
+    "url": "/ShadowArts/static/css/main.628d5fb5.chunk.css"
+  },
+  {
+    "revision": "562a5f456d526c37f5ad",
+    "url": "/ShadowArts/static/js/2.93bc4bce.chunk.js"
+  },
+  {
+    "revision": "9cdd264d58821d8a8819662b3b7dbcdb",
+    "url": "/ShadowArts/static/js/2.93bc4bce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "962d757e2961d40353ad",
+    "url": "/ShadowArts/static/js/3.4d243666.chunk.js"
+  },
+  {
+    "revision": "845e8d27723635e15c74",
+    "url": "/ShadowArts/static/js/main.68e84aad.chunk.js"
+  },
+  {
+    "revision": "67b8d69838a06b7baa6b",
+    "url": "/ShadowArts/static/js/runtime-main.224d3760.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f4acaafcec53d78db606ccf8dd346df3.js"
+  "/ShadowArts/precache-manifest.a86e32ad3f29edfc64fd875afbdedda0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ShadowArts/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
